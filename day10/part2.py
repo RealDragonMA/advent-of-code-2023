8f5e277ae7f5f9be7d61313a3af68b6e48f7805b
Mathis Mazoyer <mathis.mazoyer@etu.univ-smb.fr>
2023-12-11 21:19:06 +0100

    day11

---
+++ day10/part2.py
@@ -0,0 +1,128 @@
+from utils.fileUtils import getLines
+
+lines = []
+
+d = {
+    "|": {
+        "L": [],
+        "R": [],
+        "D": ["J", "L", "|", "S"],
+        "U": ["7", "F", "|", "S"]
+    },
+    "L": {
+        "L": [],
+        "R": ["-", "7", "J", "S"],
+        "D": [],
+        "U": ["7", "|", "F", "S"]
+    },
+    "J": {
+        "L": ["-", "F", "L", "S"],
+        "R": [],
+        "D": [],
+        "U": ["|", "7", "F", "S"]
+    },
+    "7": {
+        "L": ["-", "F", "L", "S"],
+        "R": [],
+        "D": ["|", "L", "J", "S"],
+        "U": []
+    },
+    "F": {
+        "L": [],
+        "R": ["7", "-", "J", "S"],
+        "D": ["|", "J", "L", "S"],
+        "U": []
+    },
+    "-": {
+        "L": ["L", "-", "F", "S"],
+        "R": ["7", "-", "J", "S"],
+        "D": [],
+        "U": []
+    },
+    "S": {
+        "L": ["L", "-", "F"],
+        "R": ["-", "7", "J"],
+        "D": ["|", "L", "J"],
+        "U": ["|", "7", "F"]
+    }
+}
+
+
+def getPossible(coord):
+    x, y = coord
+    possibles = []
+    symbol = getSymbol(coord)
+    if symbol == ".":
+        return []
+    directions = {
+        "L": (x - 1, y),
+        "R": (x + 1, y),
+        "D": (x, y + 1),
+        "U": (x, y - 1)
+    }
+    for direction in directions:
+        _coord = directions[direction]
+        _symbol = str(getSymbol(_coord))
+        if _symbol in d[symbol][direction] and direction not in possibles:
+            possibles.append(direction)
+    return possibles
+
+
+def getSymbol(coord):
+    x, y = coord
+    if x < 0 or x > len(lines[0]) - 1 or y < 0 or y > len(lines) - 1:
+        return "."
+    return lines[y][x]
+
+
+def reverse(direction):
+    directions = {
+        "L": "R",
+        "D": "U",
+        "R": "L",
+        "U": "D"
+    }
+    return directions[direction]
+
+
+def getNext(coord, direction):
+    x, y = coord
+    directions = {
+        "L": (x - 1, y),
+        "R": (x + 1, y),
+        "D": (x, y + 1),
+        "U": (x, y - 1)
+    }
+    return directions[direction]
+
+
+def getStarting():
+    for y in range(0, len(lines)):
+        for x in range(0, len(lines)):
+            if getSymbol((x, y)) == 'S':
+                return x, y
+
+
+def solve():
+    global lines
+    lines = getLines("input.txt")
+    s_coords = getStarting()
+    directions = getPossible(s_coords)
+    roads = []
+    for dir in directions:
+        moves = []
+        currentCoord = getNext(s_coords, dir)
+        direction = dir
+        while getSymbol(currentCoord) != "S":
+            possibles = getPossible(currentCoord)
+            possibles.remove(reverse(direction))
+            if len(possibles) == 0:
+                break
+            direction = possibles[0]
+            currentCoord = getNext(currentCoord, direction)
+            moves.append(currentCoord)
+        roads.append(moves)
+    print(sum(len(r) for r in roads))
+
+
+solve()
---
+++ day11/part1.py
@@ -0,0 +1,42 @@
+from utils.fileUtils import getLines
+
+lines = []
+
+
+def getDoubles():
+    rows = [y for y in range(len(lines)) if all(c == "." for c in lines[y])]
+    columns = [[row[x] for row in lines] for x in range(len(lines))]
+    columns = [i for i in range(len(columns)) if all(c == "." for c in columns[i])]
+    return rows, columns
+
+
+def distance_entre_tuples(coord1, coord2, doubles):
+    x1, y1 = coord1
+    x2, y2 = coord2
+    rows, columns = doubles
+    expandRows = len([x for x in columns if x2 < x < x1 or x2 > x > x1])
+    expandColumns = len([y for y in rows if y2 < y < y1 or y2 > y > y1])
+    return (abs(x2 - x1)-expandRows) + (abs(y2 - y1)-expandColumns) + expandColumns*2 + expandRows*2
+
+
+def getGalaxies():
+    coords = []
+    for y in range(len(lines)):
+        for x in range(len(lines)):
+            if lines[y][x] == "#":
+                coords.append((x, y))
+    return coords
+
+
+def solve():
+    global lines
+    lines = getLines("input.txt")
+    doubles = getDoubles()
+    galaxies = getGalaxies()
+    c = []
+    for i in range(len(galaxies)):
+        for j in range(i+1, len(galaxies)):
+            c.append(distance_entre_tuples(galaxies[i], galaxies[j], doubles))
+    print(sum(c))
+
+solve()
